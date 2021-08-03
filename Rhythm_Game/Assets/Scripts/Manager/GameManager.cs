da7fa08180b8970d66ef35cc10b9e144e83aabbf
unis <uni423@naver.com>
2021-08-03 14:39:42 +0900

    New Unity Project
    Unity Project File

---
+++ Rhythm_Game/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    //매니져 스크립트 선언
+
+    public bool IsGamePlay = false;
+
+    public int score = 0;
+    public int DeadMonsters = 0;
+
+    public void Ingame_Init()
+    {
+        Debug.Log("GameManager Init Start");
+
+        if (SceneManager.GetActiveScene().name == "02_Ingame")
+        {
+            //매니져 스크립트 할당 및 Init
+
+            IsGamePlay = true;
+        }
+    }
+}
---
+++ Rhythm_Game/Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    [SerializeField] AudioClip[] clips;
+
+    Dictionary<string, AudioClip> clipDic;
+    AudioSource sfxPlayer;
+    AudioSource bgmPlayer;
+
+    float sfxVolume = 1f;
+    float bgmVolume = 1f;
+
+    new void Awake()
+    {
+        base.Awake();
+
+        sfxPlayer = GetComponent<AudioSource>();
+        bgmPlayer = transform.GetChild(0).GetComponent<AudioSource>();
+        clipDic = new Dictionary<string, AudioClip>();
+
+        foreach (AudioClip clip in clips)
+        {
+            clipDic.Add(clip.name, clip);
+        }
+    }
+
+    public void Start()
+    {
+        StartCoroutine(Fade("in"));
+        bgmPlayer.Play();
+    }
+
+    public void PlaySound(string clipname, bool stop = true)
+    {
+        if (!clipDic.ContainsKey(clipname))
+        { Debug.Log("coludn't find sound"); return; }
+
+        if (stop) StopSFX();
+
+        sfxPlayer.PlayOneShot(clipDic[clipname], sfxVolume);
+    }
+
+    public GameObject LoopSound(string clipname)
+    {
+        if (!clipDic.ContainsKey(clipname))
+        { Debug.Log("coludn't find sound"); return null; }
+
+        GameObject loop = new GameObject("Loop");
+        AudioSource source = loop.AddComponent<AudioSource>();
+
+        source.clip = clipDic[clipname];
+        source.volume = bgmVolume;
+        source.loop = true;
+
+        StartCoroutine(Fade("in"));
+        source.Play();
+
+        return loop;
+    }
+
+    public void StopBGM()
+    {
+        StartCoroutine(Fade("out"));
+        bgmPlayer.Stop();
+    }
+
+    public void StopSFX()
+    {
+        StartCoroutine(Fade("out"));
+        sfxPlayer.Stop();
+    }
+
+    public void PlayBGM(string name = null)
+    {
+        if (name != null)
+        {
+            if (!clipDic.ContainsKey(name))
+            { Debug.Log("coludn't find sound"); return; }
+            else
+                bgmPlayer.clip = clipDic[name];
+        }
+        StartCoroutine(Fade("In"));
+        bgmPlayer.Play();
+    }
+
+    public void SetSFX(float volume)
+    {
+        sfxVolume = volume;
+    }
+
+    public void SetBGM(float volume)
+    {
+        bgmVolume = volume;
+        bgmPlayer.volume = volume;
+    }
+
+    IEnumerator Fade(string fadekind)
+    {
+        // BGM Base
+        float curVolume = 0f;
+
+        while (curVolume <= bgmVolume)
+        {
+            if (fadekind == "In")
+                bgmPlayer.volume = curVolume;
+            if (fadekind == "Out")
+                bgmPlayer.volume -= curVolume;
+
+            curVolume += Time.deltaTime;
+            yield return null;
+        }
+
+        bgmPlayer.volume = bgmVolume;
+    }
+
+}
